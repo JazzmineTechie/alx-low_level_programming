5dd98804809541392e06bc3bd7b1c015e5e6d3c6
JazzmineTechie <zeeilcreativehub@gmail.com>
2023-07-20 19:35:24 -0700

    checks for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,8 +5,10 @@
  * @c: the character
  * Return: 1 if uppercase, 0 if not
  */
+
 int _isupper(int c)
 {
+
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-int _isupper(int c)
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -10,6 +10,5 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void print_triangle(int size);
 
 #endif
