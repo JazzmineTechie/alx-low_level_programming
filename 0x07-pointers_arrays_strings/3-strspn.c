d21a9d17fa5de0ebcedacf662b6671f335409a8b
JazzmineTechie <zeeilcreativehub@gmail.com>
2023-07-31 14:05:18 -0700

    a function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: the string
  * @accept: the prefix
  *
- * Return: the number of bytes in the initial segment of s 
+ * Return: the number of bytes in the initial segment of s
  * which consist only of bytes from accept
  */
 
